592ba2ae48a60c1e69c926961a1018626c51b4ad
ali3v <xalisoglu@gmail.com>
2025-11-11 11:28:58 +0000

    test

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,16 +1,2 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    """
-    Creates a copy of the string, removing the character at position n.
-    
-    Args:
-        str: The input string
-        n: The position of the character to remove (0-based)
-        
-    Returns:
-        A new string with the character at position n removed,
-        or the original string if n is negative or out of bounds.
-    """
-    if n < 0 or n >= len(str):
-        return str
-    return str[:n] + str[n + 1:]
+print("{}".format(''.join(chr(i) for i in range(122, 96, -1))), end='')
