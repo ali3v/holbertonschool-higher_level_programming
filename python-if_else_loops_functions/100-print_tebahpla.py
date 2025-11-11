a8d03268e8fe5d311fc8eb80a237a0ab6cad942a
ali3v <xalisoglu@gmail.com>
2025-11-11 11:39:31 +0000

    test

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-s = ''.join(chr(122-i).upper() if i % 2 else chr(122-i) 
-            for i in range(26))
-print("{}".format(s), end='')
+print("{}".format(''.join([
+    chr(122 - i).upper() if i % 2 else chr(122 - i)
+    for i in range(26)
+])), end='')
