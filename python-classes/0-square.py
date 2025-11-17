8376c417766e5fb3eafe982fa434b5d0cd6bebce
ali3v <xalisoglu@gmail.com>
2025-11-17 13:26:54 +0000

    test

---
+++ python-classes/0-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class 0-square
+
+This class defines a square.
+"""
+
+
+class Square:
+
+    """
+    Defines a square.
+
+    Args:
+
+    Raises:
+
+    Returns:
+        None.
+    """
