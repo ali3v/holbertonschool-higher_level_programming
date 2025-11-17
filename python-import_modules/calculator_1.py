579556c316f24424c3b2c6c5666090eee5772eee
ali3v <xalisoglu@gmail.com>
2025-11-17 08:59:35 +0000

    test

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Return the sum of a and b"""
+    return a + b
+
+def sub(a, b):
+    """Return the difference of a and b"""
+    return a - b
+
+def mul(a, b):
+    """Return the product of a and b"""
+    return a * b
+
+def div(a, b):
+    """Return the integer division of a by b"""
+    return a // b
