bcb30ddfa4f6ef1f74733b05119f569b094d3908
ali3v <xalisoglu@gmail.com>
2025-11-17 12:38:43 +0000

    test

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Returns a tuple with the length of the sentence and its first character."""
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return (len(sentence), first_char)
