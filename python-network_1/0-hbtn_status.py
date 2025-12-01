7b53a9d591f1d70e395ed6b5f54b1f1e25835bf1
ali3v <xalisoglu@gmail.com>
2025-12-01 07:30:32 +0000

    test

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -12,18 +12,16 @@ def fetch_status():
     Fetches https://intranet.hbtn.io/status and prints details about the body.
     """
     url = "https://intranet.hbtn.io/status"
-    headers = {
-        "cfclearance": "true"
-    }
+    headers = {"cfclearance": "true"}
 
     req = request.Request(url, headers=headers)
 
     with request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(body)))
-        print("    - content: {}".format(body))
-        print("    - utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 
 if __name__ == "__main__":
