e9c3ad0142e32e6e28929671c80f9e73d0c4c2ee
ali3v <xalisoglu@gmail.com>
2025-12-01 06:52:28 +0000

    test

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,50 +1,19 @@
-#!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status using urllib.
-urllib.request is used for fetching URLs [2].
-"""
-import urllib.request
-import urllib.error
+k#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using urllib"""
 
+from urllib import request
 
 
 def fetch_status():
-    """
-    Fetches the status URL and prints the body response details.
-    """
-    url = 'https://intranet.hbtn.io/status'
-
-
-
-    headers = {
-        'cfclearance': 'true'
-    }
-
-
-    req = urllib.request.Request(url, headers=headers)
-
-    print("Body response:")
-
-    try:
-
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-
-            utf8_content = content.decode('utf-8')
-
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(utf8_content))
-
-    except urllib.error.URLError as e:
-
-        if hasattr(e, 'reason'):
-
-            print("\t- Serverə çatmaq mümkün olmadı: {}".format(e.reason))
-        elif hasattr(e, 'code'):
-
-            print("\t- Sorğu yerinə yetirilə bilmədi. Kod: {}".format(e.code))
+    url = "https://intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf-8')))
 
 
 if __name__ == "__main__":
     fetch_status()
+
