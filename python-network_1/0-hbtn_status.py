b6f9a6775564b80573fea529685a13e4a848cfa6
ali3v <xalisoglu@gmail.com>
2025-12-01 06:50:43 +0000

    test

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python3
 """
 Python script that fetches https://intranet.hbtn.io/status using urllib.
+urllib.request is used for fetching URLs [2].
 """
 import urllib.request
-import urllib.error 
+import urllib.error
+
+
 
 def fetch_status():
     """
     Fetches the status URL and prints the body response details.
     """
     url = 'https://intranet.hbtn.io/status'
-    
+
+
+
     headers = {
         'cfclearance': 'true'
     }
 
+
     req = urllib.request.Request(url, headers=headers)
-    
+
     print("Body response:")
-    
+
     try:
+
         with urllib.request.urlopen(req) as response:
             content = response.read()
-            
+
             utf8_content = content.decode('utf-8')
-    
+
             print("\t- type: {}".format(type(content)))
             print("\t- content: {}".format(content))
             print("\t- utf8 content: {}".format(utf8_content))
 
     except urllib.error.URLError as e:
+
         if hasattr(e, 'reason'):
-            print("\t- Serverə çatmaq mümkün olmadı. Səbəb: {}".format(e.reason))
+
+            print("\t- Serverə çatmaq mümkün olmadı: {}".format(e.reason))
         elif hasattr(e, 'code'):
-            print("\t- Sorğu yerinə yetirilə bilmədi. HTTP Səhv Kodu: {}".format(e.code))
+
+            print("\t- Sorğu yerinə yetirilə bilmədi. Kod: {}".format(e.code))
 
 
 if __name__ == "__main__":
